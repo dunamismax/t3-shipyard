7cd4f9a7f63769bfc354c36e648daa6984994471
dunamismax <dunamismax@tutamail.com>
2025-07-01 02:30:10 -0400

    Fix: Corrected formatting in packages/config-eslint/nextjs.js

--- packages/config-eslint/nextjs.js
+++ packages/config-eslint/nextjs.js
@@ -2,7 +2,7 @@
 module.exports = {
   extends: ['next', 'turbo', 'prettier'],
   rules: {
-    ' @next/next/no-html-link-for-pages': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
   },
   parserOptions: {
     babelOptions: {
