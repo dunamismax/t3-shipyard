2dd3c62a328c3d8fe6596520b8c549b58fac1a64
dunamismax <dunamismax@tutamail.com>
2025-07-01 04:38:45 -0400

    chore: Remove unused config-tailwind package

--- packages/config-tailwind/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss'
-
-export default {
-  content: [''], // Will be overridden by consumer app
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config
