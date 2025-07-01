3d0b451e425a0f22a79423784bfa3b8de4c252d1
dunamismax <dunamismax@tutamail.com>
2025-07-01 09:21:54 -0400

    feat(admin): migrate to app router

---
+++ apps/admin/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import '../styles/globals.css';
+
+import { Inter } from 'next/font/google';
+
+import { TRPCReactProvider } from '~/trpc/react';
+import Navbar from '../components/Navbar';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Admin App',
+  description: 'T3 Shipyard Admin App',
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={`${inter.className}`}>
+      <body>
+        <TRPCReactProvider>
+          <Navbar />
+          {children}
+        </TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
